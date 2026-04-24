34f824092d4a5625adb73f8488054400fd62471b
Centinela Code <centinela1024tb@gmail.com>
2026-04-23 21:37:48 -0600

    [ADD] create the main component for show gifs, for now use  fake data

---
+++ src/GifsApp.tsx
@@ -0,0 +1,51 @@
+import { mockGifs } from "./mock-data/gifs.mock"
+
+export const GifsApp = () => {
+   return (
+      <>
+         {/* header */}
+         <div className="content-center">
+            <h1>Buscador de Gifs</h1>
+            <p>Descubre y comparte el gif perfecto</p>
+         </div>
+
+         {/* Search */}
+         <div className="search-container">
+            <input 
+               type="text"
+               placeholder="Buscar Gifs"               
+            />
+            <button>Buscar</button>
+         </div>
+
+         {/* Previus Search */}
+         <div className="previous-searches">
+            <h2>Busquedas Previas</h2>            
+            <ul className="previous-searches-list">
+               <li>Goku</li>
+               <li>Saitama</li>
+               <li>Veggeta</li>
+               <li>Elden Ring</li>
+            </ul>
+         </div>
+         
+         {/* Gifs */}
+         <div className="gifs-container">
+            {
+               mockGifs.map( (gif) => (
+                  <div key={ gif.id } className="gif-card" >
+                     <img src={ gif.url } alt={ gif.title } />
+                     <h3>{ gif.title }</h3>
+                     <p>
+                        { gif.width }x{ gif.height } (1.5MB)
+                     </p>
+                  </div>
+               ))
+            }
+         </div>
+
+      </>
+   )
+}
+
+
